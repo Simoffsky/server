42f69272b035580ab1183a256b70fe6cf3e0d5de
Simoffsky <maks-kirsanov26@mail.ru>
2025-03-02 23:31:41 +1000

    MDEV-12302 Fix nullptr fk depth checking

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -8647,8 +8647,9 @@ ha_innobase::update_row(
 		DBUG_RETURN(HA_ERR_RECORD_IS_THE_SAME);
 	} else {
     que_thr_t *thr = que_fork_get_first_thr(m_prebuilt->upd_graph);
-    if (table->pos_in_table_list->prelocking_placeholder
-        != TABLE_LIST::PRELOCK_FK) {
+    if (table->pos_in_table_list == nullptr ||
+		table->pos_in_table_list->prelocking_placeholder != TABLE_LIST::PRELOCK_FK
+	) {
       thr->fk_cascade_depth = 0;
     }
 		if (m_prebuilt->upd_node->is_delete) {
@@ -8776,8 +8777,9 @@ ha_innobase::delete_row(
 	}
 
   que_thr_t *thr = que_fork_get_first_thr(m_prebuilt->upd_graph);
-  if (table->pos_in_table_list->prelocking_placeholder
-      != TABLE_LIST::PRELOCK_FK) {
+  if (table->pos_in_table_list == nullptr ||
+	  table->pos_in_table_list->prelocking_placeholder != TABLE_LIST::PRELOCK_FK
+	) {
     thr->fk_cascade_depth = 0;
   }
 
